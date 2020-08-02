4d89ae0bcb1898c726be1f5e7bc25a1651ee5a0d
Przemek G <PrzemekG@gmail>
2020-08-02 21:32:42 +0200

    update basic configuration file

---
+++ vagrant_multiple_machines_master_slave/scripts/setup.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+echo "updating OS"
+
+sudo yum install python3 -y
+sudo yum update -y
+sudo yum upgrade -y
+sudo adduser przemek
+sudo mkdir /home/przemek/.ssh
+sudo mkdir /root/.ssh
+sudo cat /tmp/id_rsa.pub >> /home/przemek/.ssh/authorized_keys
+sudo cat /tmp/id_rsa.pub >> /root/.ssh/authorized_keys
