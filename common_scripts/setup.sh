b76a377a246eeca8cea036ec57ce5e72040da89d
Przemek G <PrzemekG@gmail>
2020-08-03 11:35:23 +0200

    update basic configuration

--- common_scripts/setup.sh
+++ common_scripts/setup.sh
@@ -10,3 +10,5 @@ sudo mkdir /home/przemek/.ssh
 sudo mkdir /root/.ssh
 sudo cat /tmp/id_rsa.pub >> /home/przemek/.ssh/authorized_keys
 sudo cat /tmp/id_rsa.pub >> /root/.ssh/authorized_keys
+sudo usermod -aG wheel przemek
+sudo echo przemek:przemek | chpasswd
