92442efef34b8d4a9faafc04b8902db122a69ddf
Przemek G <PrzemekG@gmail>
2020-09-10 14:16:34 +0200

    delete user configuration

--- common_scripts/setup.sh
+++ common_scripts/setup.sh
@@ -5,10 +5,5 @@ echo "updating OS"
 sudo yum install python3 -y
 sudo yum update -y
 sudo yum upgrade -y
-sudo adduser przemek
-sudo mkdir /home/przemek/.ssh
 sudo mkdir /root/.ssh
-sudo cat /tmp/id_rsa.pub >> /home/przemek/.ssh/authorized_keys
 sudo cat /tmp/id_rsa.pub >> /root/.ssh/authorized_keys
-sudo usermod -aG wheel przemek
-sudo echo przemek:przemek | chpasswd
