ce63e7f7ba78098395d7e91cf45e5dd4632fed2b
Przemek G <PrzemekG@gmail>
2020-10-15 21:06:15 +0200

    terraform config

--- terraform/aws-t2micro.tf
+++ terraform/aws-t2micro.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region     = "us-east-2"
+  access_key = ""                           #access key for IAM user
+  secret_key = ""       #secret key for IAM user
+  region     = "eu-west-2"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-071884cefc7e770ba"
+  ami           = "ami-05c424d59413a2876"                       #AMI ID (Amazon machine images)
   instance_type = "t2.micro"
 }
