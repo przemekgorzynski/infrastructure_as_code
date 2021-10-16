1f64dbcd9a2d5ed8383eb460feec2c912b2a0fd9
Przemyslaw Gorzynski <przemyslaw.gorzynski@protonmail.com>
2021-10-16 23:57:33 +0200

    Added vnet perings

--- terraform/azure-infra/networking.tf
+++ terraform/azure-infra/networking.tf
@@ -26,3 +26,17 @@ resource "azurerm_subnet" "subnet_bastion" {
   virtual_network_name = azurerm_virtual_network.vnet_bastion.name
   address_prefixes     = ["10.100.0.0/24"]
 }
+
+# Peering
+resource "azurerm_virtual_network_peering" "vnet_prod-2-vnet_bastion" {
+  name                      = "${azurerm_virtual_network.vnet_prod.name}-2-${azurerm_virtual_network.vnet_bastion.name}"
+  resource_group_name       = azurerm_resource_group.rg_prod.name
+  virtual_network_name      = "${azurerm_virtual_network.vnet_prod.name}"
+  remote_virtual_network_id = "${azurerm_virtual_network.vnet_bastion.id}"
+}
+resource "azurerm_virtual_network_peering" "vnet_bastion-2-vnet_prod" {
+  name                      = "${azurerm_virtual_network.vnet_bastion.name}-2-${azurerm_virtual_network.vnet_prod.name}"
+  resource_group_name       = azurerm_resource_group.rg_bastion.name
+  virtual_network_name      = "${azurerm_virtual_network.vnet_bastion.name}"
+  remote_virtual_network_id = "${azurerm_virtual_network.vnet_prod.id}"
+}
