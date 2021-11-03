4e4075939a95e36c62ef9bf6caea0b037b52ec45
Przemyslaw Gorzynski <przemyslaw.gorzynski@protonmail.com>
2021-11-03 19:05:55 +0100

    update pipeline

--- terraform/azure-infra/example1/provider_credentials.tf
+++ terraform/azure-infra/example1/provider_credentials.tf
@@ -1,16 +1,16 @@
 # Authorization
 # Generate with: az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/SUBSCRIPTION_ID"
 variable "service_principal_appid" { 
-    default =  "VALUE"
+    default =  ""
 }
 
 variable "azure_subscription_tenant_id" {
-     default =  "VALUE"
+     default =  ""
 }
 variable "azure_subscription_id" {
-     default =  "VALUE"
+     default =  ""
 }
 
 variable "service_principal_password" {
-     default =  "VALUE"
+     default =  ""
 }
