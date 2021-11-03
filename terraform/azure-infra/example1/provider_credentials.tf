717e1fd3fe5d8c0d24929688b4f484d21c278ec2
Przemyslaw Gorzynski <przemyslaw.gorzynski@protonmail.com>
2021-11-03 20:50:30 +0100

    devops pipeline moved to another branch

--- terraform/azure-infra/example1/provider_credentials.tf
+++ terraform/azure-infra/example1/provider_credentials.tf
@@ -1,16 +1,16 @@
 # Authorization
 # Generate with: az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/SUBSCRIPTION_ID"
-#variable "service_principal_appid" { 
-#    default =  ""
-#}
-#
-#variable "azure_subscription_tenant_id" {
-#     default =  ""
-#}
-#variable "azure_subscription_id" {
-#     default =  ""
-#}
-#
-#variable "service_principal_password" {
-#     default =  ""
-#}
+variable "service_principal_appid" { 
+    default =  ""
+}
+
+variable "azure_subscription_tenant_id" {
+     default =  ""
+}
+variable "azure_subscription_id" {
+     default =  ""
+}
+
+variable "service_principal_password" {
+     default =  ""
+}
