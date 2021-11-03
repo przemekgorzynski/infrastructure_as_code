3768078072bbe61a534cb35b62df1c25ddf36b0d
Przemyslaw Gorzynski <przemyslaw.gorzynski@protonmail.com>
2021-11-03 20:29:47 +0100

    test

--- terraform/azure-infra/example1/main.tf
+++ terraform/azure-infra/example1/main.tf
@@ -10,8 +10,8 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id   = var.azure_subscription_id
-  tenant_id         = var.azure_subscription_tenant_id
-  client_id         = var.service_principal_appid
-  client_secret     = var.service_principal_password
+#  subscription_id   = var.azure_subscription_id
+#  tenant_id         = var.azure_subscription_tenant_id
+#  client_id         = var.service_principal_appid
+#  client_secret     = var.service_principal_password
 }
