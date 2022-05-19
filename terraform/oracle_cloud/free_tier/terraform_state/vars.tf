072cadf3dab0cd51d4349dcdd5e342ea5838e375
Przemyslaw Gorzynski <przemyslaw.gorzynski@protonmail.com>
2022-05-19 15:44:58 +0200

    k3s subnet + hosts

--- terraform/oracle_cloud/free_tier/terraform_state/vars.tf
+++ terraform/oracle_cloud/free_tier/terraform_state/vars.tf
@@ -18,13 +18,13 @@ variable "vcn_name"                         { default = "Oracle-vcn" }
 variable "vcn_is_ipv6enabled"               { default = "false" }
 variable "internet_gateway_display_name"    { default = "Internet gateway" }
 variable "route_table_display_name"         { default = "Route table" }
-variable "vcn_address_space" {
+variable "vcn_address_space"  {
   type = map(string)
   default = {
     vcn               = "10.0.0.0/16"
   }
 }
-variable "subnet_cidr_block" {
+variable "subnet_cidr_block"  {
   type = map(string)
   default = {
     piholeSubnet      = "10.0.1.0/24"
@@ -32,7 +32,7 @@ variable "subnet_cidr_block" {
     k3sSubnet         = "10.0.3.0/24"
   }
 }
-variable "subnet_names" {
+variable "subnet_names"       {
   type = map(string)
   default = {
     piholeSubnet      = "pihole_subnet"
@@ -62,18 +62,17 @@ variable "monitoring_image_id"              { default = "ocid1.image.oc1.eu-fran
 
 
 #K3S-MASTER
-variable "k3s_master_hostname"                  { default = "k3s-master-vm" }
-variable "k3s_master_shape"                     { default = "VM.Standard.A1.Flex" }
-variable "k3s_master_memory"                    { default = "4" }
-variable "k3s_master_ocpus"                     { default = "1" }
-variable "k3s_master_private_ip"                { default = "10.0.3.100" }
+variable "k3s_master_hostname"              { default = "k3s-master-vm" }
+variable "k3s_master_shape"                 { default = "VM.Standard.A1.Flex" }
+variable "k3s_master_memory"                { default = "4" }
+variable "k3s_master_ocpus"                 { default = "1" }
+variable "k3s_master_private_ip"            { default = "10.0.3.100" }
 #https://docs.oracle.com/en-us/iaas/images/ - images #Ubuntu20
-variable "k3s_image_id"                  { default = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaafofmp3otdb5fh3ged2zhsepoh3e2dkaus636uee4vpt7jrgqssma" }
+variable "k3s_image_id"                     { default = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaafofmp3otdb5fh3ged2zhsepoh3e2dkaus636uee4vpt7jrgqssma" }
 
 #K3S-WORKER
-variable "k3s_worker_hostname"                  { default = "k3s-worker1-vm" }
-variable "k3s_worker_shape"                     { default = "VM.Standard.A1.Flex" }
-variable "k3s_worker_memory"                    { default = "4" }
-variable "k3s_worker_ocpus"                     { default = "1" }
-variable "k3s_worker1_private_ip"               { default = "10.0.3.101" }
-#https://docs.oracle.com/en-us/iaas/images/ - images #Ubuntu20
+variable "k3s_worker_hostname"              { default = "k3s-worker1-vm" }
+variable "k3s_worker_shape"                 { default = "VM.Standard.A1.Flex" }
+variable "k3s_worker_memory"                { default = "4" }
+variable "k3s_worker_ocpus"                 { default = "1" }
+variable "k3s_worker1_private_ip"           { default = "10.0.3.101" }
